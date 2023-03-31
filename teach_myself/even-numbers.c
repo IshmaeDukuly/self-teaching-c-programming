2830d3b90b16aebeebf00625b7a876d88997eba6
Moisema <moisemadukuly82@gmail.com>
2023-03-31 17:08:27 +0000

    even-numbers

---
+++ teach_myself/even-numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Even_numbers - Print even numbers 
+ *
+ *Return: Always 1 (Success)
+ */
+
+int main()
+{
+	int n = 0;
+	
+	for (int n = 1; n <= 100; n++){
+		
+		n+=1;
+		printf("%d\n", n);
+	}
+	
+	return (0);
+}
