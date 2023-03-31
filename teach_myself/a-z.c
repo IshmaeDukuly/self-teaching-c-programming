9c6f107234804d66b8effe254247335ad85d5b58
Moisema <moisemadukuly82@gmail.com>
2023-03-31 16:56:57 +0000

    a-z

---
+++ teach_myself/a-z.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Start - Print  capital 'A' to 'Z'
+ * @ch: Putchar
+ * Return: Output
+ */
+
+int main()
+{
+	int letter;
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	return (0);
+}
